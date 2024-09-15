bb05e9682a72dd344b0b7e2fe01c1621d84deedf
Yogina Khandelwal <yogina.khandelwal@gmail.com>
2024-09-15 15:15:06 -0400

    initial changes for uplift page

--- client/src/pages/Uplift/Uplift.jsx
+++ client/src/pages/Uplift/Uplift.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Uplift.scss";
 
 export default function Uplift() {
   return (
-    <div>
-      <h1>Uplift page</h1>
-    </div>
-  )
+    <section className="Uplift">
+      <div className="uplift__animation">
+        <div className="container">
+          <div className="circle"></div>
+        </div>
+      </div>
+      <div className="uplift__audio">
+        <iframe
+          width="100%"
+          height="450"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/328705064&color=%23d4c6a1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        ></iframe>
+      </div>
+    </section>
+  );
 }
